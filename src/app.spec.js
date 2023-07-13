0e64af35f6db57d95efdec20a3ba3a5b57ef08de
Jossic <jossic.lapierre@gmail.com>
2023-07-13 20:36:37 +0200

    tech: initial setup

---
+++ src/app.spec.js
@@ -0,0 +1,9 @@
+import {describe, it, expect} from "vitest"
+
+describe('app', () => {
+    describe('filter', () => {
+        it('should return true', () => {
+        expect(true).toBe(true);
+        });
+    })
+})
